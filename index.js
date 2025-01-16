e6374631f399144e44fd839ac3d165ae37597361
fuadmuhtasim <jeetfuad@gmail.com>
2025-01-16 00:17:10 -0600

    allowing requests from every origin

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const jsonServer = require("json-server"); // importing json-server library
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
+const corsOptions = {
+    origin: '*',  // Allow all origins
+    credentials: true,  // Allow cookies and authentication headers
+};
+
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
+server.use(cors(corsOptions));
 server.use(middlewares);
 server.use(router);
 
